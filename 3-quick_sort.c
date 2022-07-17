5b85c4d56a776fdfc4ae4731decedf30a37e06ad
Ogbu Isreal <ogbuisreal22@gmail.com>
2022-07-17 14:57:55 +0100

    sorting Algorithms

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -20,14 +20,14 @@ recursive_quick_sort(array, size, 0, size - 1);
 */
 void recursive_quick_sort(int *array, size_t size, int start, int end)
 {
-int p;
-if (start < end)
-{
-	p = partition(array, size, start, end);
+	int p;
+	if (start < end)
+	{
+		p = partition(array, size, start, end);
 
-	recursive_quick_sort(array, size, start, p - 1);
-	recursive_quick_sort(array, size, p + 1, end);
-}
+		recursive_quick_sort(array, size, start, p - 1);
+		recursive_quick_sort(array, size, p + 1, end);
+	}
 
 }
 
@@ -41,22 +41,22 @@ if (start < end)
 */
 size_t partition(int *array, size_t size, int start, int end)
 {
-int pivot = array[end];
-int i = start - 1;
-int j;
+	int pivot = array[end];
+	int i = start - 1;
+	int j;
 
-for (j = start; j <= end - 1; j++)
-{
-	if (array[j] < pivot)
+	for (j = start; j <= end - 1; j++)
 	{
-		i++;
-		swap_int1(array, i, j);
-		print_array(array, size);
+		if (array[j] < pivot)
+		{
+			i++;
+			swap_int1(array, i, j);
+			print_array(array, size);
+		}
 	}
-}
-swap_int1(array, i + 1, end);
-print_array(array, size);
-return (i+1);
+	swap_int1(array, i + 1, end);
+	print_array(array, size);
+	return (i+1);
 }
 
 /**
