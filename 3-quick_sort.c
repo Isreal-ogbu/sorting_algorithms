fc9bb3354a6c6d61598af7bbe9fc240f1cfee431
Ogbu Isreal <ogbuisreal22@gmail.com>
2022-07-19 23:52:31 +0100

    sorting Algorithms

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,74 +1,83 @@
 #include "sort.h"
-#include <stdio.h>
-/**
- * quick_sort - sorts an array of integers in ascending order
- * using the quick sort sort algorithm
- * @array: pointer to the array
- * @size: size of the array
-*/
-void quick_sort(int *array, size_t size)
-{
-recursive_quick_sort(array, size, 0, size - 1);
-}
 
 /**
- * recursive_quick_sort - recursive part
- * @array: array to use
- * @size: size
- * @start: start index
- * @end: end index
-*/
-void recursive_quick_sort(int *array, size_t size, int start, int end)
+ * partition - Function that divide the list
+ *
+ * @array: Array of number to sort.
+ * @low: data in the index 0 of the list.
+ * @high: data in the last - 1 index of the list.
+ * @size: Size of the array.
+ * Return: the partition index.
+ */
+
+int partition(int *array, int low, int high, size_t size)
 {
-	int p;
-	if (start < end)
+	int pivot;
+	int i, j, temp1, temp2;
+
+	pivot = array[high];
+	i = (low - 1);
+
+	for (j = low; j < high; j++)
 	{
-		p = partition(array, size, start, end);
-
-		recursive_quick_sort(array, size, start, p - 1);
-		recursive_quick_sort(array, size, p + 1, end);
-	}
-
-}
-
-/**
- * partition - partition the array
- * @array: array to use
- * @size: size
- * @start: start index
- * @end: end index
- * Return: partition index
-*/
-size_t partition(int *array, size_t size, int start, int end)
-{
-	int pivot = array[end];
-	int i = start - 1;
-	int j;
-
-	for (j = start; j <= end - 1; j++)
-	{
-		if (array[j] < pivot)
+		if (array[j] <= pivot)
 		{
 			i++;
-			swap_int1(array, i, j);
-			print_array(array, size);
+			temp1 = array[i];
+			array[i] = array[j];
+			array[j] = temp1;
+			if (i != j)
+				print_array(array, size);
 		}
 	}
-	swap_int1(array, i + 1, end);
-	print_array(array, size);
-	return (i+1);
+	if (pivot < array[i + 1])
+	{
+		temp2 = array[i + 1];
+		array[i + 1] = array[high];
+		array[high] = temp2;
+		print_array(array, size);
+	}
+	return (i + 1);
 }
 
 /**
- * swap_int1 - swap variable values
- * @array: array to use
- * @a: index 1
- * @b: index 2
-*/
-void swap_int1(int *array, int a, int b)
+ * sorting - Function that sort the list
+ *
+ * @array: Array of number to sort.
+ * @low: data in the index 0 of the list.
+ * @high: data in the last - 1 index of the list.
+ * @size: Size of the array.
+ * Return: Void
+ */
+
+void sorting(int *array, int low, int high, size_t size)
 {
-int tmp;
-tmp = array[a];
-array[a] = array[b];
-array[b] = tmp;
+	int partition_number;
+
+	if (low < high)
+	{
+		partition_number = partition(array, low, high, size);
+		sorting(array, low, partition_number - 1, size);
+		sorting(array, partition_number + 1, high, size);
+	}
+}
+
+/**
+ * quick_sort -  divide-and-conquer sorting algorithm
+ *
+ * @array: Array of number to sort.
+ * @size: Size of the array.
+ * Return: Void
+ */
+
+void quick_sort(int *array, size_t size)
+{
+	int low = 0;
+	int high = size - 1;
+
+	if (size < 2)
+	{
+		return;
+	}
+	sorting(array, low, high, size);
 }
